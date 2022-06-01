078d84a49bf750309a77910a93b302e327af16be
KathiTram <49622903+KathiTram@users.noreply.github.com>
2022-06-01 14:22:58 -0700

    Update etchasketch.js

--- etchasketch.js
+++ etchasketch.js
@@ -3,9 +3,20 @@ const gridContainer = document.querySelector('.gridContainer');
 const gridSizeSlider = document.getElementById('gridsizeslider');
 const gridSizeValue = document.getElementById('gridvalue');
 
+const colorPicker = document.getElementById('colorpicker');
+
+
+const clearButton = document.querySelector('.clearbutton');
+const resetbutton = document.querySelector('.resetbutton');
+
 // Default grid size
 createBoxes(10);
+
+// Grid size value text above slider
 gridSizeValue.innerHTML = gridSizeSlider.value;
+
+// Default hover color
+colorChosen = 'black';
 
 function createBoxes(numBox) {
     for (let i = 0; i < numBox; i++) {
@@ -16,6 +27,12 @@ function createBoxes(numBox) {
         row.appendChild(square);
       }
     }
+
+    document.querySelectorAll('.box').forEach(box => {
+        box.addEventListener('mouseover', function() {
+            box.style.backgroundColor = colorChosen;
+        })
+    });
   }
 
 // Update gridsize when slider value is changed
@@ -24,3 +41,18 @@ gridSizeSlider.addEventListener('input', function() {
     gridSizeValue.innerHTML = this.value;
     createBoxes(this.value);
 });
+
+
+colorPicker.addEventListener('input', function () {
+    colorChosen = this.value;
+});
+
+clearButton.addEventListener('click', function() {
+    gridContainer.querySelectorAll('.box').forEach(box => {
+        box.style.backgroundColor = '#ffffff';
+    });
+});
+
+resetbutton.addEventListener('click', function() {
+    window.location.reload();
+})
